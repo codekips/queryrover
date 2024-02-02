2396bd1ec2b12c386ed7fed44ccb8a9d391a23fd
aarora7 <abhay_arora@intuit.com>
2024-02-02 20:21:26 +0530

    chore: remove commented data

--- qrover/__init__.py
+++
@@ -1,8 +0,0 @@
-# from rover_mgmt import RoverMgmt
-
-
-# if __name__ == "__main__":
-#     rm = RoverMgmt()
-#     ds = rm.add_dataset("equity", "/Users/aarora7/personal/code/BE/python/queryrover/tests/dumps/can_variations.csv","csv"); # type: ignore
-#     print("hello")
-#     print(str(ds.get_schema()))# type: ignore
